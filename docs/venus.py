7adad454507e7f6ce99ce58f055d920a1a752507
tom <tomgoodwin.tg@gmail.com>
2023-05-31 19:39:50 +1000

    working on venus data

--- docs/venus.py
+++ docs/venus.py
@@ -2,22 +2,61 @@
 # topography.
 
 import numpy as np
-from debiased_spatial_whittle import fit, matern
 import matplotlib.pyplot as plt
 import scipy.io
 
+from debiased_spatial_whittle.grids import RectangularGrid
+from debiased_spatial_whittle.models import MaternCovarianceModel, MaternModel
+from debiased_spatial_whittle.periodogram import Periodogram, ExpectedPeriodogram
+from debiased_spatial_whittle.likelihood import DebiasedWhittle, Estimator
+
 # load the topography data and standardize by the std
 z = scipy.io.loadmat('Frederik53.mat')['topodata']
-z = z / np.std(z)
+z = (z - np.mean(z)) / np.std(z)
+
+grid = RectangularGrid(z.shape)
+periodogram = Periodogram()
+expected_periodogram = ExpectedPeriodogram(grid, periodogram)
+debiased_whittle = DebiasedWhittle(periodogram, expected_periodogram)
+estimator = Estimator(debiased_whittle)
+
+# frequency mask corresponding to the data processing
+from numpy.fft import fftfreq
+m, n = z.shape
+x, y = np.meshgrid(fftfreq(m) * 2 * np.pi, fftfreq(n) * 2 * np.pi, indexing='ij')
+freq_norm = np.sqrt(x ** 2 + y ** 2)
+frequency_mask = freq_norm < np.pi
+print(z.shape, frequency_mask.shape)
+
+plt.figure()
+plt.imshow(frequency_mask)
+plt.show()
+
+debiased_whittle.frequency_mask = frequency_mask
 
 # plot
 plt.figure()
-plt.imshow(z, origin='lower', cmap='Spectral')
+plt.imshow(z, origin='lower', cmap='bwr')
 plt.show()
 
-init_guess = np.array([50, .5, 1])
+# periodogram plot
+from numpy.fft import fftshift
+plt.figure()
+per = periodogram(z)
+plt.imshow(fftshift(10 * np.log10(per) * frequency_mask))
+plt.show()
 
-cov = matern
-# TODO add spectral mask as in the paper to account for data processing
-est = fit(z, np.ones_like(z), cov, init_guess, fold=True)
-print(est)
+model = MaternModel()
+model.rho.init_guess=10
+model.sigma.init_guess = 1
+model.nu.init_guess = 1
+
+print(model)
+print(estimator(model, z))
+
+from debiased_spatial_whittle.simulation import SamplerOnRectangularGrid
+sampler = SamplerOnRectangularGrid(model, grid)
+z_sim = sampler()
+plt.figure()
+plt.imshow(z_sim, cmap='bwr')
+plt.show()
--- docs/venus_bayes.py
+++ docs/venus_bayes.py
@@ -16,7 +16,7 @@ from debiased_spatial_whittle.bayes import DeWhittle, Whittle, Gaussian
 
 # load the topography data and standardize by the std
 z = scipy.io.loadmat('Frederik53.mat')['topodata']
-z /= np.std(z)
+z = (z - np.mean(z)) / np.std(z)    # this is changed
 n = z.shape
 
 # plot
@@ -34,7 +34,7 @@ grid = RectangularGrid(n)
 
 
 
-model = MaternModel()
+model = MaternModel()                # try exponential model
 model.nu = 0.8860611533657291
 nugget = 0.0012308293585964476
 dw = DeWhittle(z, grid, model, nugget=nugget)
@@ -54,3 +54,19 @@ adj_dewhittle_post, A = dw.RW_MH(niter, adjusted=True, acceptance_lag=100)
 title = 'posterior comparisons'
 legend_labels = ['deWhittle', 'adj deWhittle']
 plot_marginals([dewhittle_post, adj_dewhittle_post], None, title, [r'log$\rho$', r'log$\sigma$'], legend_labels, shape=(1,2))
+
+
+
+sim_z = dw.sim_z(np.exp(dw.res.x))
+
+fig, ax = plt.subplots(2,1, figsize=(40,10))
+ax[0].set_title('Sea Temperature Data', fontsize=22)
+im1 = ax[0].imshow(z, cmap='Spectral', origin='lower')
+fig.colorbar(im1, ax=ax[0])
+
+ax[1].set_title('simulated', fontsize=22)
+im2 = ax[1].imshow(sim_z, cmap='Spectral', origin='lower')
+fig.colorbar(im2, ax=ax[1])
+    
+fig.tight_layout()
+plt.show()
