fix gradient of SquaredExponentialCovarianceModel

src/debiased_spatial_whittle/models.py
@@ -409,7 +409,7 @@ class SquaredExponentialModel(CovarianceModel):
         
         d2 = sum((lag**2 for lag in lags))
         nugget_effect = self.nugget.value*np.all(lags == 0, axis=0)
-        acf = self.sigma.value ** 2 * np.exp(- 0.5*d2 / self.rho.value ** 2) + nugget_effect  # exp(0.5) as well
+        acf = self.sigma.value ** 2 * np.exp(- 1 / 2 * d2 / self.rho.value ** 2) + nugget_effect  # exp(0.5) as well
         return acf
     
     def f(self, freq_grid:Union[list, np.ndarray], infsum_grid:Union[list, np.ndarray], d:int=2):
@@ -431,8 +431,8 @@ class SquaredExponentialModel(CovarianceModel):
         the model's parameters"""
         # TODO: include nugget
         d2 = sum((lag ** 2 for lag in lags))
-        d_rho =  2 / self.rho.value ** 3 * d2 * self.sigma.value ** 2 * np.exp(-d2 / self.rho.value ** 2)
-        d_sigma = 2 * self.sigma.value * np.exp(- d2 / self.rho.value ** 2)
+        d_rho =  self.rho.value ** (-3) * d2 * self.sigma.value ** 2 * np.exp(- 1 / 2 * d2 / self.rho.value ** 2)
+        d_sigma = 2 * self.sigma.value * np.exp(- 1 / 2 * d2 / self.rho.value ** 2)
         d_nugget = 1 * (d2 == 0)
         return np.stack((d_rho, d_sigma, d_nugget), axis=-1)
 