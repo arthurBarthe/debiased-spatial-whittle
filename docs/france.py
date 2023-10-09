2accfb81399c226c3d19326434f71f878999cdc9
arthur <ahw795@qmul.ac.uk>
2023-10-09 16:17:23 +0100

    switch to Matern covariance

--- docs/france.py
+++ docs/france.py
@@ -1,18 +1,19 @@
 import matplotlib.pyplot as plt
 
 import debiased_spatial_whittle.grids as grids
-from debiased_spatial_whittle.models import ExponentialModel, SquaredExponentialModel
+from debiased_spatial_whittle.models import ExponentialModel, SquaredExponentialModel, MaternCovarianceModel
 from debiased_spatial_whittle.grids import RectangularGrid
 from debiased_spatial_whittle.simulation import SamplerOnRectangularGrid
 from debiased_spatial_whittle.periodogram import Periodogram, ExpectedPeriodogram
 from debiased_spatial_whittle.likelihood import Estimator, DebiasedWhittle
 
-model = SquaredExponentialModel()
+model = MaternCovarianceModel()
 model.rho = 35
-model.sigma = 1
-model.nugget = 0.025
+model.sigma = 2
+model.nu = 1.5
+#model.nugget = 0.025
 
-shape = (620 * 1, 620 * 1)
+shape = (512 * 1, 512 * 1)
 mask_france = grids.ImgGrid(shape).get_new()
 grid_france = RectangularGrid(shape)
 grid_france.mask = mask_france
@@ -23,11 +24,11 @@ z = sampler()
 periodogram = Periodogram()
 expected_periodogram = ExpectedPeriodogram(grid_france, periodogram)
 debiased_whittle = DebiasedWhittle(periodogram, expected_periodogram)
-estimator = Estimator(debiased_whittle, use_gradients=True)
+estimator = Estimator(debiased_whittle, use_gradients=False)
 
-model_est = SquaredExponentialModel()
-model_est.nugget = None
-estimate = estimator(model_est, z)
+model_est = MaternCovarianceModel()
+#model_est.nugget = None
+estimate = estimator(model_est, z, opt_callback=lambda *args, **kargs: print(args))
 print(estimate)
 
 plt.imshow(z, origin='lower', cmap='Spectral')
